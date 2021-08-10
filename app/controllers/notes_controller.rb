342715e59166c19b442a2494964696345f749977
linchiquei <cclok922@gmail.com>
2021-08-10 22:48:50 +0800

    update file

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,23 +1,31 @@
 class NotesController < ApplicationController
 
     def index
+        @note = Note.order(id: :desc)
     end
 
     def new
         @note = Note.new
     end
 
+    def show
+        @note = Note.find(params[:id])
+    end
+
     def create
         #render html: params
         #params["params"] 要問為什麼可以？
-        title = params[:title] 
-        content = params[:content]
-        note = Note.new(title: title, content: content)
+        #title = params[:title] 
+        #content = params[:content]
+        #@note = Note.new(title: title, content: content)
 
-        if(note.save)
+        clear_note = params.require(:note).permit(:title, :content)
+        @note = Note.new(clear_note)
+
+        if(@note.save)
             redirect_to "/notes" #這是蛇沒意思？
         else
-            #待處理
+            render :new
         end
     end
 end
